5b6adfb1f9ab6141328649ac6cb6668fda9862b6
Bryanorih <bryanorihuela2459@gmail.com>
2023-04-19 02:23:34 -0700

    bruh

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const periodBtn = document.querySelector(".period")
 let mathOperator = document.querySelectorAll(".mathOperator")
 let calcLog = document.querySelector(".Calculator-log")
 const totalNum = document.querySelector(".totalSum")
+const ceButton = document.querySelector(".CE")
 
 let operators = ["+","-","/","*"]
 let lastNumInInput = input.textContent[input.textContent.length-1]
@@ -14,6 +15,14 @@ let lastNumInLog = calcLog.textContent[calcLog.textContent.length-1]
 
 //NOTE - CLEARS THE INPUT
 clearButton.addEventListener('click', ()=>{
+    input.removeAttribute('id')
+    input.setAttribute("id", "reset");
+    input.textContent = "0"
+    calcLog.textContent = ""
+})
+
+
+ceButton.addEventListener('click', ()=>{
     input.removeAttribute('id')
     input.setAttribute("id", "reset");
     input.textContent = "0"
@@ -43,7 +52,7 @@ backSpace.addEventListener('click', () => {
 
 //NOTE - PLUS OR NEGATIVE IN THE INPUT
 plusMinusBtn.addEventListener('click', () => {
-    console.log(input.textContent[0]);
+    // console.log(input.textContent[0]);
     switch(input.textContent[0]){
         case "0":
             break;
@@ -58,13 +67,17 @@ plusMinusBtn.addEventListener('click', () => {
 
 //NOTE - DECIMAL POINT IN INPUT
 periodBtn.addEventListener('click', () =>{
-    input.textContent.includes(".") ? input.textContent : input.textContent = input.textContent.concat(".")
+    input.textContent.includes(".") ? input.textContent : input.textContent = input.textContent + "."
+    input.removeAttribute('id')
 })
 
 //NOTE - MATH OPERATOR BUTTON FUNCTION
 for( let btn of mathOperator){
     btn.addEventListener('click', () =>{
-        if(operators.some(operator => calcLog.textContent.includes(operator)) && input != 0){
+        if(calcLog.textContent.includes("=")){
+            calcLog.textContent = input.textContent + btn.textContent
+            input.setAttribute("id", "total");
+        }else if(operators.some(operator => calcLog.textContent.includes(operator)) && input != 0){
             // console.log(calcLog.textContent, input.textContent);
             input.textContent = eval(calcLog.textContent.concat(input.textContent))
             calcLog.textContent = input.textContent + btn.textContent
@@ -79,13 +92,16 @@ for( let btn of mathOperator){
 
 //NOTE - EQUALS BUTTON FUNCTION
 totalNum.addEventListener('click', () =>{
+    // const getPosition = (string, subString, index) =>{
+    //     return string.split(subString, index).join(subString).length;
+    // }
     if(calcLog.textContent.includes("=")){
-        for(let operator of operators){
-            calcLog.textContent = calcLog.textContent.substring(calcLog.textContent.indexOf(operator));
-            calcLog.textContent = input.textContent + calcLog.textContent
-            input.textContent = eval(calcLog.textContent.substr(0,calcLog.textContent.length-1))
-            break;
-        }
+        let lastOperator = [calcLog.textContent.lastIndexOf("-"),calcLog.textContent.lastIndexOf("+"),calcLog.textContent.lastIndexOf("*"),calcLog.textContent.lastIndexOf("/")]
+        lastOperator = Math.max(...lastOperator)
+        console.log(lastOperator);
+        calcLog.textContent = calcLog.textContent.substring(lastOperator,calcLog.textContent.length -1);
+        calcLog.textContent = input.textContent + calcLog.textContent + "="
+        input.textContent = eval(calcLog.textContent.substring(0,calcLog.textContent.length-1))
     }else{
         calcLog.textContent = calcLog.textContent + input.textContent + "=";
         input.textContent = eval(calcLog.textContent.substr(0,calcLog.textContent.length-1))
